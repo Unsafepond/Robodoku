6ffbf402a7e1a07b337ab58be40dc705ed4c7b81
Jerrod Junker <unsafepond@gmail.com>
2015-07-16 09:26:48 -0600

    some refactoring and detects solved puzzles

--- solver.rb
+++ solver.rb
@@ -1,9 +1,16 @@
 require 'pry'
 class Solver
-	def rows(puzzle)
+	attr_accessor :puzzle, :squares, :rows, :colunms
+	def initialize(puzzle)
+		@puzzle = puzzle
+		@squares = squares(puzzle)
+		@rows = rows(puzzle)
+		@colunms = colunms(puzzle)
+	end
+	def rows(puzzle = puzzle)
 		puzzle.split("\n")
 	end
-	def colunms(puzzle)
+	def colunms(puzzle = puzzle)
 		colunms = ["", "", "", "", "", "", "", "", ""]
 		rows(puzzle).each do |row|
 			colunms.each_with_index do |i, index|
@@ -12,72 +19,49 @@ class Solver
 		end
 		return colunms
 	end
-	def squares(puzzle)
-		squares = [[], [], [], [], [], [], [], [], []]
+	def squares(puzzle = puzzle)
+		squares_list = [[], [], [], [], [], [], [], [], []]
 		rows = rows(puzzle)
-		squares[0] << rows[0][0..2]
-		squares[0] << rows[1][0..2]
-		squares[0] << rows[2][0..2]
-
-		squares[1] << rows[0][3..5]
-		squares[1] << rows[1][3..5]
-		squares[1] << rows[2][3..5]
-
-		squares[2] << rows[0][6..8]
-		squares[2] << rows[1][6..8]
-		squares[2] << rows[2][6..8]
-
-		squares[3] << rows[3][0..2]
-		squares[3] << rows[4][0..2]
-		squares[3] << rows[5][0..2]
-
-		squares[4] << rows[3][3..5]
-		squares[4] << rows[4][3..5]
-		squares[4] << rows[5][3..5]
-
-		squares[5] << rows[3][6..8]
-		squares[5] << rows[4][6..8]
-		squares[5] << rows[5][6..8]
-
-		squares[6] << rows[6][0..2]
-		squares[6] << rows[7][0..2]
-		squares[6] << rows[8][0..2]
-
-		squares[7] << rows[6][3..5]
-		squares[7] << rows[7][3..5]
-		squares[7] << rows[8][3..5]
-
-		squares[8] << rows[6][6..8]
-		squares[8] << rows[7][6..8]
-		squares[8] << rows[8][6..8]
-		return squares
+		rows[0..2].each do |row| 
+			squares_list[0] << row[0..2]
+			squares_list[1] << row[3..5]
+			squares_list[2] << row[6..8]
+		end
+		rows[3..5].each do |row|
+			squares_list[3] << row[0..2]
+			squares_list[4] << row[3..5]
+			squares_list[5] << row[6..8]
+		end
+		rows[6..8].each do |row|
+			squares_list[6] << row[0..2]
+			squares_list[7] << row[3..5]
+			squares_list[8] << row[6..8]
+		end
+		return squares_list.map { |square| square.join}
 	end
 	def spot_filler(row)
-		until row.include?(" ") == false && solved? 
+		until row.include?(" ") == false 
 			row.sub!(" ", Random.rand(10).to_s)
-			# binding.pry
 		end
+		return row
 	end
 	def blank_spot_detector(puzzle)
-		filled = [] 
-		rows(puzzle).each do |row|
+		rows(puzzle).map do |row|
 			if row.include?(" ")
-				filled << spot_filler(row)
-			else
-				filled << row
+				spot_filler(row)
 			end
 		end
 	end
-	def solved?
+	def solved?(puzzle)
 		solved = false
-    if sum_of_squares && sum_of_rows && sum_of_colunms
+    if sum_of(squares) && sum_of(rows) && sum_of(colunms)
     	solved = true
     end
     return solved
 	end
-	def sum_of_colunms(colunms)
-		characters = colunms.map do |colunm|
-			colunm.split("")
+	def sum_of(var)
+		characters = var.map do |x|
+			x.split("")
 		end
 		numbers = characters.flatten.map do |char|
 			if char == " "
@@ -90,34 +74,21 @@ class Solver
 
 		return sum / 9 == 45
 	end
-	def sum_of_rows(rows)
-		characters = rows.map do |row|
-			row.split("")
-		end
-		numbers = characters.flatten.map do |char|
-			if char == " "
-				char = 0
+	def solve
+		if solved?(puzzle)
+			return puzzle
+		else
+			if solved?(new_solution) == false
+				solved?(new_solution)
 			else
-				char.to_i
+				return new_solution
 			end
 		end
-		sum = numbers.inject(:+)
-
-		return sum / 9 == 45
 	end
-	def sum_of_squares(squares)
-		characters = squares.map do |square|
-			square.join.split("")
-		end
-		numbers = characters.flatten.map do |char|
-			if char == " "
-				char = 0
-			else
-				char.to_i
-			end
-		end
-		sum = numbers.inject(:+)
-
-		return sum / 9 == 45
+	def combine(puzzle_array)
+		puzzle_array.join("\n")
+	end
+	def new_solution
+		combine(blank_spot_detector(puzzle))
 	end
 end
